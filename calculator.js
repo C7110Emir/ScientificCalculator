9498079083f437ba055ad94d5cd0f79a53eb168d
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-08 21:13:36 -0400

    sample added

---
+++ calculator.js
@@ -0,0 +1,34 @@
+const screen = document.getElementById('screen')
+const ac = document.getElementById("Acbutton")
+const pi = document.getElementById("pibutton")
+const square = document.getElementById("squarebutton")
+const sqrt = document.getElementById("sqrtbutton")
+const log = document.getElementById('logbutton')
+const factorial = document.getElementById("factorialbutton")
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+const four = document.getElementById("four")
+const five = document.getElementById("five")
+const six = document.getElementById("six")
+const seven = document.getElementById("seven")
+const eight = document.getElementById("eight")
+const nine = document.getElementById("nine")
+const zero = document.getElementById("zero")
+const equal = document.getElementById("equal")
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const divide = document.getElementById("division")
+const multiply = document.getElementById("mult")
+const dot = document.getElementById("dot")
+const sin = document.getElementById("sinus")
+const cos = document.getElementById("cosinus")
+const tan = document.getElementById("tangent") 
+
+
+
+
+
+
+
+
